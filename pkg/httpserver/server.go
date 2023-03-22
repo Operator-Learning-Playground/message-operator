598ccd1246450abbc2d9988230c6e61a14d9e030
googs1025 <googs1025@gmail.com>
2023-03-23 00:47:44 +0800

    feat: http server build

---
+++ pkg/httpserver/server.go
@@ -0,0 +1,28 @@
+package httpserver
+
+import "github.com/gin-gonic/gin"
+
+func HttpServer() error {
+	router := gin.New()
+	defer func() {
+
+	}()
+
+	register(router)
+	err := router.Run(":8888")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func register(router *gin.Engine) {
+
+	r := router.Group("/v1")
+	{
+		//
+		r.POST("/send", SendTo)
+
+	}
+}
+
